7796d930e3d23793c2917045e31be62ce04a2fca
Bharatsolanky <bharat650@gmail.com>
2021-10-01 13:03:16 -0500

    Main file

--- main.py
+++ main.py
@@ -5,12 +5,28 @@ See assignment-01.pdf for details.
 # no imports needed.
 
 def foo(x):
-    ### TODO
-    pass
+    if x <= 1:
+        return x
+    else:
+        ra, rb = foo(x-1),foo(x-2)
+        return ra + rb
 
 def longest_run(mylist, key):
-    ### TODO
-    pass
+    num = 0
+    temp1 = 0
+    temp2 = 0
+    for i in range(len(mylist)):
+        if mylist[i] == key:
+            temp1 += 1
+        else:
+            if (temp2 < temp1):
+                temp2 = temp1
+                temp1 = 0
+            else:
+                temp1 = 0
+
+    num = temp2
+    return num
 
 
 class Result:
@@ -20,18 +36,30 @@ class Result:
         self.right_size = right_size             # run on right side of input
         self.longest_size = longest_size         # longest run in input
         self.is_entire_range = is_entire_range   # True if the entire input matches the key
-        
+
     def __repr__(self):
         return('longest_size=%d left_size=%d right_size=%d is_entire_range=%s' %
               (self.longest_size, self.left_size, self.right_size, self.is_entire_range))
-    
-    
+
 def longest_run_recursive(mylist, key):
-    ### TODO
-    pass
+    if len(mylist) == 1:
+        if mylist[0] == key:
+            return Result(1, 1, 1, True)
+        else:
+            return Result(0, 0, 0, False)
+    Result1 = longest_run_recursive(mylist[:len(mylist) // 2], key)
+    Result2 = longest_run_recursive(mylist[len(mylist) // 2:], key)
+    is_entire_range = Result1.is_entire_range and Result2.is_entire_range
+    left_side = Result1.left_size + (1 if Result1.is_entire_range else 0) * Result2.left_size
+    right_side = Result2.right_size + (1 if Result2.is_entire_range else 0) * Result1.right_size
+    longest_size = max(Result1.longest_size, Result2.longest_size, Result1.right_size + Result2.left_size)
+    return Result(left_side, right_side, longest_size, is_entire_range)
+
+
 
 ## Feel free to add your own tests here.
 def test_longest_run():
     assert longest_run([2,12,12,8,12,12,12,0,12,1], 12) == 3
 
-
+def test_longest_run_recursive():
+    assert longest_run_recursive([2,12,12,8,12,12,12,0,12,1], 12).longest_size == 3
